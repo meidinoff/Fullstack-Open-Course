074d230f85278f9d12fb0c0f9560f8ec897e3bc8
Max Eidinoff <meidinoff@gmail.com>
2024-09-26 23:05:02 -0400

    Up to 3b

--- part3/index.js
+++ part3/index.js
@@ -1,5 +1,8 @@
 const express = require('express')
+const cors = require('cors')
+
 const app = express()
+app.use(cors())
 
 let notes = [
     {
@@ -19,7 +22,20 @@ let notes = [
     }
 ]
 
+const requestLogger = (request, response, next) => {
+    console.log('Method:', request.method)
+    console.log('Path: ', request.path)
+    console.log('Body: ', request.body)
+    console.log('---')
+    next()
+}
+
 app.use(express.json())
+app.use(requestLogger)
+
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+}
 
 app.get('/', (request, response) => {
     response.send('<h1>Hello World!</h1>')
@@ -74,7 +90,9 @@ app.delete('/api/notes/:id', (request, response) => {
     response.status(204).end()
 })
 
-const PORT = 3001
+app.use(unknownEndpoint)
+
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
