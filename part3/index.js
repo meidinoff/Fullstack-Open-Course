d0a53ea7b4912251bd13fa14a686b21666771fda
Max Eidinoff <meidinoff@gmail.com>
2024-10-08 16:32:07 -0400

    More backend modifications

--- part3/index.js
+++ part3/index.js
@@ -6,7 +6,6 @@ import Note from './models/note.js'
 
 const app = express()
 app.use(cors())
-app.use(express.static('dist'))
 
 const notes = [
     {
@@ -49,7 +48,7 @@ app.get('/api/notes', (request, response) => {
     })
 })
 
-app.get('/api/notes/:id', (request, response) => {
+app.get('/api/notes/:id', (request, response, next) => {
     Note.findById(request.params.id)
         .then(note => {
             if (note) {
@@ -58,10 +57,7 @@ app.get('/api/notes/:id', (request, response) => {
                 response.status(404).end()
             }
         })
-        .catch(error => {
-            console.log(error)
-            response.status(400).send({ error: 'malformatted id' })
-        })  
+        .catch(error => next(error))
 })
 
 app.post('/api/notes', (request, response) => {
@@ -83,14 +79,43 @@ app.post('/api/notes', (request, response) => {
     })
 })
 
-app.delete('/api/notes/:id', (request, response) => {
-    const id = request.params.id
-    notes = notes.filter(note => note.id !== id)
+app.put('/api/notes/:id', (request, response, next) => {
+    const body = request.body
 
-    response.status(204).end()
+    const note = {
+        content: body.content,
+        important: body.important,
+    }
+
+    Note.findByIdAndUpdate(request.params.id, note, {
+        new: true })
+            .then(updatedNote => {
+                response.json(updatedNote)
+            })
+            .catch(error => next(error))
+})
+
+app.delete('/api/notes/:id', (request, response) => {
+    Note.findByIdAndDelete(request.params.id)
+        .then(result => {
+            response.status(204).end()
+        })
+        .catch(error => next(error))
 })
 
 app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' })
+    }
+
+    next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
