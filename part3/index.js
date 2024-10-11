72a9a6332280c5163d9bb5fa29b96cfe2a410103
Max Eidinoff <meidinoff@gmail.com>
2024-10-11 08:56:51 -0400

    Added ESlint

--- part3/index.js
+++ part3/index.js
@@ -7,21 +7,6 @@ import Note from './models/note.js'
 const app = express()
 app.use(cors())
 
-const notes = [
-    {
-        "content": "HTML is Easy",
-        "important": "true"
-    },
-    {
-        "content": "Mongoose make use of mongo easy",
-        "important": "true"
-    },
-    {
-        "content": "Callback-functions suck",
-        "important": "true"
-    }
-]
-
 const requestLogger = (request, response, next) => {
     console.log('Method:', request.method)
     console.log('Path: ', request.path)
@@ -60,39 +45,29 @@ app.get('/api/notes/:id', (request, response, next) => {
         .catch(error => next(error))
 })
 
-app.post('/api/notes', (request, response) => {
+app.post('/api/notes', (request, response, next) => {
     const body = request.body
-
-    if (!body.content) {
-        return response.status(400).json({
-            error: 'content missing'
-        })
-    }
     
     const note = new Note({
         content: body.content,
         important: body.important || false
     })
 
-    note.save().then(savedNote => {
-        response.json(savedNote)
-    })
+    note.save()
+        .then(savedNote => {
+            response.json(savedNote)
+        })
+        .catch(error => next(error))
 })
 
 app.put('/api/notes/:id', (request, response, next) => {
-    const body = request.body
+    const { content, important } = request.body
 
-    const note = {
-        content: body.content,
-        important: body.important,
-    }
-
-    Note.findByIdAndUpdate(request.params.id, note, {
-        new: true })
-            .then(updatedNote => {
-                response.json(updatedNote)
-            })
-            .catch(error => next(error))
+    Note.findByIdAndUpdate(request.params.id, { content, important }, { new: true, runValidators: true, context: 'query' })
+        .then(updatedNote => {
+            response.json(updatedNote)
+        })
+        .catch(error => next(error))
 })
 
 app.delete('/api/notes/:id', (request, response) => {
@@ -110,6 +85,8 @@ const errorHandler = (error, request, response, next) => {
 
     if (error.name === 'CastError') {
         return response.status(400).send({ error: 'malformatted id' })
+    } else if (error.name === 'ValidationError') {
+        return response.status(400).json({ error: error.message })
     }
 
     next(error)
