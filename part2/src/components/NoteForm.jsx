512639a72931d4e48728c1f25d9838e1c3a84149
Max Eidinoff <meidinoff@gmail.com>
2024-11-06 09:11:01 -0500

    Finished Part 5c

--- part2/src/components/NoteForm.jsx
+++ part2/src/components/NoteForm.jsx
@@ -3,6 +3,10 @@ import { useState } from 'react'
 const NoteForm = ({ createNote }) => {
     const [newNote, setNewNote] = useState('')
 
+    const handleChange = (event) => {
+        setNewNote(event.target.value)
+    }
+
     const addNote = (event) => {
         event.preventDefault()
         createNote({
@@ -20,7 +24,7 @@ const NoteForm = ({ createNote }) => {
             <form onSubmit={addNote}>
                 <input
                     value={newNote}
-                    onChange={event => setNewNote(event.target.value)}
+                    onChange={handleChange}
                 />
                 <button type="submit">save</button>
             </form>
